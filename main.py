63f1de389d6068db5b49b91077cb16c0c43f49ee
Mohsen <83723914+mohsenrahmdeli@users.noreply.github.com>
2024-10-08 09:59:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,19 @@ import pytesseract
 from docx import Document
 import os
 
-# Setting the Tesseract OCR path
 pytesseract.pytesseract.tesseract_cmd = r'Your  tesseract.exe file directory here' 
 
-# Image file path
 image_path = r'Your Image file path'
 
-# Read file name and extension
 image_name, image_extension = os.path.splitext(os.path.basename(image_path))
 
-# Read text from image
 res = pytesseract.image_to_string(image_path)
 
-# Create a new Word document
 doc = Document()
 doc.add_paragraph(res)
 
-# The path to save the Word file
 output_path = os.path.join(os.path.dirname(image_path), f'{image_name}.docx')
 
-# Save the Word document
 doc.save(output_path)
 
 print(f'Text successfully in file {output_path} saved.')
